e36c1c3782d0d55a88ec73ea35fdc5613b42a2aa
Angelina01 <ch.maurice@hotmail.fr>
2023-04-03 11:13:17 +0200

    Slider
    slider

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ window.addEventListener("blur",()=>{
 window.addEventListener("focus",()=>{
     document.title = docTitle;
 })
+
+
+/****** AUTOMATIC SLIDER ********/
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
