048c384626900ffd34cad922711f241edf51a75d
Angelina01 <ch.maurice@hotmail.fr>
2023-04-25 14:19:54 +0200

    map update

--- script.js
+++ script.js
@@ -66,3 +66,21 @@ function fadeIn() {
 } 
 
 
+/************************** Text animation *******************************/
+const elements = document.querySelectorAll('.paragraph');
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: .4
+}
+const callbacks = (entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting){
+      entry.target.classList.add('active');
+    }
+  });
+}
+let observer = new IntersectionObserver(callbacks, options);
+elements.forEach(element => {
+  observer.observe(element);
+});
